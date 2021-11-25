59ca529e13b0a3d4a73743b94b49e6e83948c8fd
Salman Ansari <salman98101@gmail.com>
2021-11-26 00:12:24 +0530

    started

--- rn-first-app/App.js
+++ rn-first-app/App.js
@@ -1,21 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+  FlatList,
+} from "react-native";
 
 export default function App() {
+  const [entergoal, setEntergoal] = useState("");
+  const [courcegoal, setCourcegoal] = useState([]);
+  const inputhandler = (enteredtext) => {
+    setEntergoal(enteredtext);
+  };
+  const addGoalHandler = () => {
+    // setCourcegoal((currentGoals) => [...currentGoals, entergoal]);
+    if (entergoal !== "") {
+      setCourcegoal([...courcegoal, entergoal]);
+      // setEntergoal("");
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View style={styles.inputcontainer}>
+        <TextInput
+          placeholder="Add Goals"
+          style={styles.textinput}
+          onChangeText={inputhandler}
+          value={entergoal}
+        />
+        <View style={{ padding: 10 }}>
+          <Button title="ADD" onPress={addGoalHandler} />
+        </View>
+      </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {courcegoal.map((goal, index) => (
+          <View key={index} style={styles.listitem}>
+            <Text>{goal}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50,
+  },
+  inputcontainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  textinput: {
+    borderBottomColor: "black",
+    borderWidth: 1,
+    padding: 5,
+    width: "80%",
+  },
+  listitem: {
+    padding: 10,
+    backgroundColor: "#ccc",
+    marginVertical: 10,
+    borderColor: "black",
+    borderWidth: 1,
   },
 });
