483f385eba7f1b3fc93b41e34729f540082aabf5
Salman Ansari <salman98101@gmail.com>
2021-12-04 20:04:35 +0530

    chnages

---
+++ guessnumber/screens/StartGame.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+
+const StartGame = () => {
+  return (
+    <View style={styles.screen}>
+      <Text>Welcome to The Game Screen</Text>
+      <View style={styles.inputcontainer}>
+        <Text>Select a Number</Text>
+        <TextInput />
+        <View style={styles.buttoncontainer}>
+          <Button title="Reset" />
+          <Button title="Confirm" />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default StartGame;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+  },
+  inputcontainer: {},
+  buttoncontainer: {},
+});
