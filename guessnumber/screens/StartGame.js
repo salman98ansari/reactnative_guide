0ccf2c71b499aed3d3650ae84b56c2cd1699861e
Salman Ansari <salman98101@gmail.com>
2021-12-07 16:24:39 +0530

    buttons

--- guessnumber/screens/StartGame.js
+++ guessnumber/screens/StartGame.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 const StartGame = () => {
   return (
     <View style={styles.screen}>
-      <Text>The Game Screen</Text>
+      <Text style={styles.title}>Thee Game Screen</Text>
       <View style={styles.inputcontainer}>
         <Text>Select a Number</Text>
         <TextInput />
@@ -22,9 +22,19 @@ export default StartGame;
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    padding: 10,
+    padding: 15,
     alignItems: "center",
   },
-  inputcontainer: {},
-  buttoncontainer: {},
+  title: { fontSize: 18, marginVertical: 10 },
+  inputcontainer: {
+    width: 300,
+    maxWidth: "80%",
+    alignItems: "center",
+  },
+  buttoncontainer: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    paddingHorizontal: 15,
+  },
 });
