50fdb8a27d1c57e1bf5d4841c5956e65072c06d1
Salman Ansari <salman98101@gmail.com>
2021-12-25 18:27:34 +0530

    react navigations

---
+++ meals/navigation/MealsNavigator.js
@@ -0,0 +1,13 @@
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import CategoriesScreen from "../screens/CategoriesScreen";
+import CategoryMealScreen from "../screens/CategoryMealScreen";
+import MealDetailScreen from "../screens/MealDetailScreen";
+
+const MealsNavigator = createStackNavigator({
+  Categories: CategoriesScreen,
+  CategoryMeals: CategoryMealScreen,
+  MealDetail: MealDetailScreen,
+});
+
+export default createAppContainer(MealsNavigator);
--- mealsapp/App.js
+++ mealsapp/App.js
@@ -1,5 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+
+// const fetchFonts = () => {
+//   return Font.loadAsync({
+//     "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+//     "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+//   });
+// };
 
 export default function App() {
   return (
